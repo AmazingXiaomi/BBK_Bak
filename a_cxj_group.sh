2c14a47a5cec47d9f739f5212f315f9740c5bd47
xiaomi <1205938863@qq.com>
2022-10-28 16:43:59 +0800

    更新

--- a_cxj_group.sh
+++ a_cxj_group.sh
@@ -4,6 +4,7 @@
 # export Proxy_Url="代理网址 例如：星空、熊猫 生成选择txt 一次一个"
 # export CXJ_GROUP_PINS="jd_xxx&jd_xxx"# 指定pin组队车头，多个用英文&分割
 # export CXJ_GROUP_COUNT="30" # 每队多少人, 默认30人或者满员 可不填
+# exprot CXJ_GROUP_BAN_PINS="jd_xxx&jd_xxx" #跳过黑号pin
 # 支持CXJ_GROUP_PINS 或者 链接方式(`cxj_group_list.txt`存放需要组队的URL,一行一个)
 pwd
 _ftype=""
